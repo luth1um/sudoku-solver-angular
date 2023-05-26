4fb9ba572eea61fed53923987f9b74a1dd1d41c3
luth1um <52032867+luth1um@users.noreply.github.com>
2023-05-26 18:46:25 +0200

    changed order of e2e tests

--- cypress/e2e/all-tests.cy.ts
+++ cypress/e2e/all-tests.cy.ts
@@ -1,5 +1,5 @@
-import './next-number-solver.cy.ts';
 import './spec.cy.ts';
+import './next-number-solver.cy.ts';
 import './sudoku-invalid-entries.cy.ts';
 import './sudoku-solver.cy.ts';
 import './sudoku-unsolvable.cy.ts';
