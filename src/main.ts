11482148aaf99b60a8a4bd7b35d4a8fe9265c580
luth1um <52032867+luth1um@users.noreply.github.com>
2026-03-09 16:44:28 +0100

    removed deprecated provider

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
 import { TranslateModule } from '@ngx-translate/core';
@@ -11,7 +10,6 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideAnimationsAsync(),
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: provideTranslateHttpLoader({ prefix: './assets/i18n/', suffix: '.json' }),
